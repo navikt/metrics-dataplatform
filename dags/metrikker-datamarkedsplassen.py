0a364c8d2f66f0db6a49eefaddda1844bb3446d4
Erik Vattekar <erik.vattekar@nav.no>
2022-05-20 13:54:44 +0200

    add on failure slack notification to k8s operator tasks

--- dags/metrikker-datamarkedsplassen.py
+++ dags/metrikker-datamarkedsplassen.py
@@ -1,10 +1,12 @@
 import os
+from re import S
 from airflow import DAG
 from datetime import datetime, timedelta
 from airflow.contrib.operators import kubernetes_pod_operator
 from airflow.providers.slack.operators.slack_webhook import SlackWebhookOperator
 
 PIPELINE_NAME = "metrikker-datamarkedsplassen"
+SLACK_CHANNEL = "#nada-composer-info"
 TASK_MAX_RETRIES = 2
 TASK_RETRY_DELAY = timedelta(seconds=5)
 TASK_STARTUP_TIMEOUT = 360
@@ -20,6 +22,20 @@ ENVS = {
     "DATAPRODUCTS_TABLE": "nada-prod-6977.bq_metrics_datamarkedsplassen.dataproducts"
 }
 
+
+def on_task_error(context):
+    slack_notification = SlackWebhookOperator(
+        task_id="airflow_task_failed",
+        http_conn_id=None,
+        webhook_token=os.environ["SLACK_WEBHOOK_TOKEN"],
+        message=f"@here DAG {PIPELINE_NAME} feilet kl. {datetime.now().isoformat()}.",
+        channel=SLACK_CHANNEL,
+        link_names=True,
+        icon_emoji=":sadpanda:",
+    )
+    slack_notification.execute(context)
+
+
 with DAG(PIPELINE_NAME,
          start_date=datetime(year=2022, month=5, day=20),
          schedule_interval="0 5 * * *",
@@ -32,7 +48,7 @@ with DAG(PIPELINE_NAME,
         task_id="slack-start-notification",
         webhook_token=os.environ["SLACK_WEBHOOK_TOKEN"],
         message=f"Pipeline _*{PIPELINE_NAME}*_ startet",
-        channel="#nada-composer-info",
+        channel=SLACK_CHANNEL,
         link_names=True,
         icon_emoji=":dataverk:",
         retries=2,
@@ -48,6 +64,7 @@ with DAG(PIPELINE_NAME,
         retry_delay=TASK_RETRY_DELAY,
         namespace="composer-2-0-13-airflow-2-2-5-3b8c2b10",
         startup_timeout_seconds=TASK_STARTUP_TIMEOUT,
+        on_failure_callback=on_task_error,
         is_delete_operator_pod=DELETE_POD_ON_COMPLETED,
     )
 
@@ -61,6 +78,7 @@ with DAG(PIPELINE_NAME,
         retry_delay=TASK_RETRY_DELAY,
         namespace="composer-2-0-13-airflow-2-2-5-3b8c2b10",
         startup_timeout_seconds=TASK_STARTUP_TIMEOUT,
+        on_failure_callback=on_task_error,
         is_delete_operator_pod=DELETE_POD_ON_COMPLETED,
     )
 
@@ -70,7 +88,7 @@ with DAG(PIPELINE_NAME,
         task_id="success_notification",
         webhook_token=os.environ["SLACK_WEBHOOK_TOKEN"],
         message=f"Vellykket kjøring av pipeline _*{PIPELINE_NAME}*_",
-        channel="#nada-composer-info",
+        channel=SLACK_CHANNEL,
         link_names=True,
         icon_emoji=":dataverk:",
         retries=3,
